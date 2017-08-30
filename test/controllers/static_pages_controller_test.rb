126e5d7a8b402e2cfc348f20a5927d9faca784dd
ThreeSugar <threesugar@hotmail.com>
2017-08-31 00:27:38 +0800

    Finished Static Pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,12 +4,17 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @base_title = "Ruby On Rails Tutorial Sample App"
   end
-  
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
     assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
 
   test "should get help" do
     get static_pages_help_url
