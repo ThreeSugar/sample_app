74b1cf3c186501da5eec38912c3a09e2d349aba6
ThreeSugar <threesugar@hotmail.com>
2017-09-01 15:31:05 +0800

    finished layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
-  root :to => 'static_pages#home'
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
-
+  get '/signup', to: 'users#new'
 
  
 end
